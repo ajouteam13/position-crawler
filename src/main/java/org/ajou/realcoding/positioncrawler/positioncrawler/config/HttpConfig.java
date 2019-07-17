1ba268c25a143bb5ef62c28adac80453ae56f2a6
rohguentak <rhgt78@gmail.com>
2019-07-17 18:37:20 +0900

    add config and domain

---
+++ src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/config/HttpConfig.java
@@ -0,0 +1,4 @@
+package org.ajou.realcoding.positioncrawler.positioncrawler.config;
+
+public class HttpConfig {
+}
---
+++ src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/domain/EncryptedSummonerId.java
@@ -0,0 +1,4 @@
+package org.ajou.realcoding.positioncrawler.positioncrawler.domain;
+
+public class EncryptedSummonerId {
+}
