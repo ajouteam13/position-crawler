5f336f8ba79b4f18684ae3270ce8f69b052c1841
rohguentak <rhgt78@gmail.com>
2019-07-17 18:45:25 +0900

    config에 http,mongo,swager추가 및 domain에 암호화된id와 leagueposition데이터형 추가

--- src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/domain/LeaguePosition.java
+++ src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/domain/LeaguePosition.java
@@ -1,4 +1,32 @@
 package org.ajou.realcoding.positioncrawler.positioncrawler.domain;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
 public class LeaguePosition {
+    private String queueType;
+    private String summonerName;
+    private boolean hotStreak;
+    private MiniSeriesDTO miniSeries;
+    private int wins;
+    private boolean veteran;
+    private int losses;
+    private String rank;
+    private String leagueId;
+    private boolean inactive;
+    private boolean freshBlood;
+    private String tier;
+    private String summonerId;
+    private int leaguePoints;
+    @Id
+    private String id;
+
+    @Data
+    private static class MiniSeriesDTO {
+        private String progress;
+        private int losses;
+        private int target;
+        private int wins;
+    }
 }
